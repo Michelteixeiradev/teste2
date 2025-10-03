c6442178a9514ad4688f040a70ee928cfb5296c8
= <michelkleyton63@gmail.com>
2025-10-03 14:52:37 -0300

    esta dando erro

--- main/loja.py
+++ main/loja.py
@@ -1,4 +1,10 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.properties import StringProperty, ListProperty
+
+class MusicControl(BoxLayout):
+    devices = ([])
+    connected_device = ListProperty()
+
 
